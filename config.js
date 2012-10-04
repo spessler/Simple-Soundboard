a1bf6cfe22ad7da31bed89692e31f8b03a44bcf5
Ben Spessler <spessler@gmail.com>
2012-10-04 14:25:48 -0400

    Initial Commit

---
+++ config.js
@@ -0,0 +1,207 @@
+  var sounds = [
+    {
+       name: 'Bill',
+       file: 'divider',
+       sid: 'bill',
+     },
+
+     {
+       name: 'Tomorrow',
+       file: 'bill/tomorrow.wav',
+       sid: 'bill',
+    },
+       
+     {
+       name: 'What that is',
+       file: 'bill/what_that_is.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'No Words There',
+       file: 'bill/words.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'No Words There 2',
+       file: 'bill/no_words.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Play Us Out',
+       file: 'bill/play_us_out.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'What Does That Mean',
+       file: 'bill/what_does_that_mean.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Dont Know What That Means',
+       file: 'bill/dont_know_what_that_mean.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Cant Do It',
+       file: 'bill/cant_do_it.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Do It Live',
+       file: 'bill/do_it_live.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Fuck It',
+       file: 'bill/fuck_it.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Sucks',
+       file: 'bill/sucks.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'End The Show',
+       file: 'bill/endshow.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'There It Is',
+       file: 'bill/there_is_is.wav',
+       sid: 'bill',
+     },
+     {
+       name: 'Go',
+       file: 'bill/go.wav',
+       sid: 'bill',
+     },
+      {
+       name: 'Hartman',
+       file: 'divider',
+       sid: 'hartman',
+     },
+     {
+       name: 'Children That Lived',
+       file: 'hartman/children_that_lived.wav',
+       sid: 'hartman',
+     },
+
+
+     {
+       name: 'Misc',
+       file: 'divider',
+       sid: 'misc',
+     },
+	   {
+       name: 'Antoine',
+       file: 'misc/hidekids.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'Pigfuckers',
+       file: 'misc/soul.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'Treasure',
+       file: 'misc/treasure.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'Suck A Fuck',
+       file: 'misc/suckafuck.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'Fuck Your Own Face',
+       file: 'misc/fuckface.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'What The Fuck Is The Internet',
+       file: 'misc/internet.wav',
+       sid: 'misc',
+     },
+	   {
+       name: 'Trolololo',
+       file: 'misc/trolololo_beginning.mp3',
+       sid: 'misc',
+     },
+     {
+       name: 'Samuel L',
+       file: 'divider',
+       sid: 'sam',
+     },
+	   {
+       name: 'Tasty Burger',
+       file: 'sam/burger.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'Break Your Concentration',
+       file: 'sam/concentration.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'What Ain\'t No Country',
+       file: 'sam/no_country.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'Speak English In What',
+       file: 'sam/english.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'English Do You Speak It',
+       file: 'sam/speakeng.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'Say What Again',
+       file: 'sam/what_again.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'One More Time',
+       file: 'sam/onemore.wav',
+       sid: 'sam',
+     },
+	   {
+       name: 'Allow Me To Retort',
+       file: 'sam/retort.wav',
+       sid: 'sam',
+     },
+     {
+       name: 'South Carolina',
+       file: 'divider',
+       sid: 'south',
+     },
+	   {
+       name: 'Personally Believe',
+       file: 'south/believe.wav',
+       sid: 'south',
+     },
+	   {
+       name: 'Iraq',
+       file: 'south/iraq.wav',
+       sid: 'south',
+     },
+	   {
+       name: 'Maps',
+       file: 'south/maps.wav',
+       sid: 'south',
+     },
+	   {
+       name: 'Such As',
+       file: 'south/suchas.wav',
+       sid: 'south',
+     },
+	   {
+       name: 'US Americans',
+       file: 'south/usamericans.wav',
+       sid: 'south',
+     },
+     ];
+
